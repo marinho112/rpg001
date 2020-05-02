4c790ba972478f8ba933795cc5b76b36739f6e8d
marinho112 <34494111+marinho112@users.noreply.github.com>
2020-05-01 21:39:52 -0300

    multiplos alvos, ataques e golpes

--- script/areas/combate.gd
+++ script/areas/combate.gd
@@ -1,26 +1,46 @@
 extends Node2D
 
-
+#Listas 
 var listaAmigos = [0,1,2,3,4,5]
 var listaInimigos = [0,1,2,3,4,5]
-var tamanhoAmigo
-var tamanhoInimigo
 var listaAmigosObjeto = []
 var listaInimigosObjeto = []
 var listaTurnos = []
+var vetorSelecionados = []
+var listaAmigosSelecionados=[]
+var listaInimigosSelecionados=[]
+var listaCursoresSobressalentes=[]
+
+#Preloads
 var pre_personagem = preload("res://nodes/objetos/personagem_combate.tscn")
+var pre_cursor = preload("res://nodes/objetos/cursor.tscn")
+
+#Variaveis Inteiras
+var tamanhoAmigo
+var tamanhoInimigo
+var numSelecao =1
+var acaoPretendida
+var valorAcaoPretendida
+
+
+#Variaveis de Controle
 var controleDeTurno = true
+var mudarLadoSelecao=false
+var selecaoAliados=false
+var selecaoIndividual=true
+var emSelecao=false
+var posicaoCursor = 0
+var ocilante= 0
+var clicavel = true
+
+#Contadores
 var turno=0
 var rodada=0
 var numPersonagens= len(listaAmigos) + len(listaInimigos)
-var mudarLadoSelecao=false
-var selecaoAliados=false
-var posicaoCursor = 0
+var numSelecionados =0
+
+#objetos 
 var atacante
-var acaoPretendida
-var valorAcaoPretendida
-var emSelecao=false
-var ocilante= 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -102,34 +122,84 @@ func ordenaTurno():
 
 func emSelecao(delta):
 	if(emSelecao):
+		var lista=[]
 		if(selecaoAliados):
 			$cursor.set_frame(0)
 			desenhaCursorSelecao(listaAmigosObjeto,delta)
+			lista = listaAmigosSelecionados
 		else:
 			$cursor.set_frame(1)
 			desenhaCursorSelecao(listaInimigosObjeto,delta)
-	
-		if Input.is_action_just_pressed("ui_up"):
-			if(posicaoCursor>0):
-				posicaoCursor-=1
-			else:
-				posicaoCursor=5
-				
-		if Input.is_action_just_pressed("ui_down"):
-			if(posicaoCursor<5):
-				posicaoCursor+=1
-			else:
-				posicaoCursor=0
-				
-		if Input.is_action_just_pressed("a"):
-			emSelecao=false
-			$cursor.set_visible(false)
-			if(selecaoAliados):
-				chamaAcao(listaAmigosObjeto[posicaoCursor])
-			else:
-				chamaAcao(listaInimigosObjeto[posicaoCursor])
+			lista = listaInimigosSelecionados
+			
+		var repetir=true
 		
+		if Input.is_action_just_pressed("ui_up"):
+			if(!clicavel):
+				clicavel=true
+			while(repetir):
+				repetir=false
+				if(posicaoCursor>0):
+					posicaoCursor-=1
+				else:
+					posicaoCursor=5
+				for i in len(lista):
+					if(posicaoCursor==lista[i]):
+						repetir=true
+		
+		repetir=true
+		if Input.is_action_just_pressed("ui_down"):
+			if(!clicavel):
+				clicavel=true
+			while(repetir):
+				repetir=false
+				if(posicaoCursor<5):
+					posicaoCursor+=1
+				else:
+					posicaoCursor=0
+				for i in len(lista):
+					if(posicaoCursor==lista[i]):
+						repetir=true
+		
+		if (Input.is_action_just_pressed("a") and clicavel):
+			if(selecaoIndividual):
+				var novoCursor = pre_cursor.instance()
+				novoCursor.set_position($cursor.get_position())
+				novoCursor.set_sprite_frames($cursor.get_sprite_frames())
+				novoCursor.set_animation($cursor.get_animation())
+				novoCursor.set_frame($cursor.get_frame())
+				novoCursor.set_visible(true)
+				add_child(novoCursor)
+				clicavel=false
+				
+				listaCursoresSobressalentes.append(novoCursor)
+				if(selecaoAliados):
+					listaAmigosSelecionados.append(posicaoCursor)
+				else:
+					listaInimigosSelecionados.append(posicaoCursor)
+				
+			if(numSelecionados<numSelecao):
+				numSelecionados+=1
+				if(selecaoAliados):
+					vetorSelecionados.append(listaAmigosObjeto[posicaoCursor])
+				else:
+					vetorSelecionados.append(listaInimigosObjeto[posicaoCursor])
+			if(numSelecionados>=numSelecao):
+				if(!clicavel):
+					clicavel=true
+					
+				emSelecao=false
+				$cursor.set_visible(false)
+				chamaAcao(vetorSelecionados)
+				listaAmigosSelecionados=[]
+				listaInimigosSelecionados=[]
+				for i in len(listaCursoresSobressalentes):
+					listaCursoresSobressalentes[i].queue_free()
+				listaCursoresSobressalentes=[]
+		var posicaoAntiga = posicaoCursor
 		if Input.is_action_just_pressed("ui_right"):
+			if(!clicavel):
+				clicavel=true
 			if(selecaoAliados and (posicaoCursor<3)):
 				posicaoCursor += 3
 			elif(posicaoCursor>=3):
@@ -138,6 +208,8 @@ func emSelecao(delta):
 				selecaoAliados=true
 				posicaoCursor=0
 		elif Input.is_action_just_pressed("ui_left"):
+			if(!clicavel):
+				clicavel=true
 			if(!selecaoAliados and (posicaoCursor<3)):
 				posicaoCursor += 3
 			elif(posicaoCursor>=3):
@@ -145,6 +217,12 @@ func emSelecao(delta):
 			elif(selecaoAliados and mudarLadoSelecao):
 				selecaoAliados=false
 				posicaoCursor=0
+				
+		for i in len(lista):
+			if(posicaoCursor==lista[i]):
+				posicaoCursor=posicaoAntiga
+				break
+		
 		
 			pass
 
@@ -167,34 +245,39 @@ func desenhaCursorSelecao(lista,delta):
 	$cursor.set_position(Vector2(positionElemento.x + ajusteTamanho, positionElemento.y))
 	
 	
-func chamaAcao(alvo):
+func chamaAcao(alvos):
 	# Atacar : 0
 	# Usar Item : 1
 
 	match acaoPretendida:
 		
 		0:
-			atacante.atacar(alvo,valorAcaoPretendida)
+			atacante.atacar(alvos,valorAcaoPretendida)
 
-func seleciona(tipoSelecao,tipoAcao,codAcao,fonte):
+func seleciona(tipoSelecao,tipoAcao,codAcao,numSelecao,fonte):
 	
 	posicaoCursor = 0
 	acaoPretendida=tipoAcao
 	valorAcaoPretendida=codAcao
 	atacante=fonte
 	emSelecao=true
+	vetorSelecionados=[]
+	self.numSelecao=numSelecao
+	numSelecionados=0
 	$cursor.set_visible(true)
-	
-	if(tipoSelecao>1):
+	if((tipoSelecao|61)==(63)):
 		mudarLadoSelecao=true
 	else:
 		mudarLadoSelecao=false
-	
-	if((tipoSelecao%2) == 0):
-		selecaoAliados = false
-	else:
+	if((tipoSelecao|62)==(63)):
 		selecaoAliados = true
-		
+	else:
+		selecaoAliados = false
+	if((tipoSelecao|59)==(63)):
+		selecaoIndividual=true
+	else:
+		selecaoIndividual=false
+	
 	pass
 	
 	
--- script/objetos/personagem_combate.gd
+++ script/objetos/personagem_combate.gd
@@ -5,11 +5,17 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var personagem
-var moverAtaque=false
+var moverAtaque=true
+var animacaoAtaque = true
+var controlaAtaque = false
+var voltando = true
 var speed = 200
-var atacado 
+var atacados=[]
 var listaInimigos= []
 var posicaoInicial
+var ataquesRealizados=0
+var golpesRealizados = 0
+var golpesPorAtaque=2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,23 +25,41 @@ func _ready():
 
 func _process(delta):
 	
-	moverAtaque(delta)
+	controlaAtaque(delta)
 	
 
 func alteraPosicaoPermanente(posicao):
 	set_position(posicao)
 	posicaoInicial=posicao
 
-func atacar(atacado,tipoAtaque):
+func atacar(atacados,tipoAtaque):
 	self.listaInimigos=get_parent().listaInimigosObjeto
-	self.atacado=atacado
-	moverAtaque=true
+	self.atacados=atacados
+	ataquesRealizados=0
+	controlaAtaque=true
 	pass
+
+func controlaAtaque(delta):
+	if(controlaAtaque):
+		if(ataquesRealizados<len(atacados)):
+			moverAtaque(delta)
+			animacaoAtaque(delta)
+			
+			
+		else:
+			animacaoVoltar(delta)
+			if(!voltando):
+				$AnimatedSprite.set_animation("default")
+				$AnimationPlayer.stop()
+				controlaAtaque=false
+				get_parent().terminaTurno()
+			
 
 func moverAtaque(delta):
 	if(moverAtaque):
 		$AnimatedSprite.set_animation("movendo")
-		var inimigoAtacado = atacado
+		$AnimatedSprite.set_flip_h(true)
+		var inimigoAtacado = atacados[ataquesRealizados]
 		var posiAtacado=inimigoAtacado.get_position()
 		var frames =inimigoAtacado.get_node("AnimatedSprite").get_sprite_frames()
 		var anim = inimigoAtacado.get_node("AnimatedSprite").get_animation()
@@ -44,43 +68,27 @@ func moverAtaque(delta):
 		var position= get_position()
 		var distanciaX= (atacadoTamanho.x + posiAtacado.x) - (position.x)
 		var distanciaY= ((posiAtacado.y) - (position.y) )
-		var moduloX=0
-		var moduloY=0
-		var moveX=0
-		var moveY=0
 		
-		if(distanciaX>=0):
-			moduloX=distanciaX
-		else:
-			moduloX=-distanciaX
-			
-		if(distanciaY>=0):
-			moduloY=distanciaY
-		else:
-			moduloY=-distanciaY
 		
-		if((moduloX>=10)or(moduloY >=10)):
-			if(moduloY<=1):
-				moduloY=1
-			if(moduloX<=1):
-				moduloX=1
-			if(moduloX>moduloY):
-				moveX= distanciaX/moduloX
-				moveY= distanciaY/moduloX
-			else:
-				moveX = distanciaX/moduloY
-				moveY= distanciaY/moduloY
-			
-			set_position(Vector2(position.x+(moveX*delta*(speed+moduloX)),position.y+(moveY*delta*(speed+moduloY))))
-		else:
+		
+		if(mover(distanciaX,distanciaY,delta)):
 			moverAtaque=false
-			$AnimatedSprite.set_animation("default")
-			set_position(posicaoInicial)
-			get_parent().terminaTurno()
+			$AnimatedSprite.set_flip_h(false)
+			
 		
 	pass
 	
-func animacaoAtaque():
+func animacaoAtaque(delta):
+	
+	if(golpesRealizados>=golpesPorAtaque):
+		moverAtaque=true
+		ataquesRealizados+=1
+		golpesRealizados=0
+	
+	if(!moverAtaque and animacaoAtaque):
+		$AnimatedSprite.set_animation("batendo")
+		$AnimationPlayer.play("atacando")
+		
 	pass
 
 func calculaDano():
@@ -97,6 +105,54 @@ func animacaoApanhar():
 	pass
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func animacaoVoltar(delta):
+	if(voltando):
+		$AnimatedSprite.set_animation("movendo")
+		$AnimatedSprite.set_flip_h(false)
+		var position= get_position()
+		var distanciaX= (posicaoInicial.x) - (position.x)
+		var distanciaY= ((posicaoInicial.y) - (position.y) )
+	
+		if(mover(distanciaX,distanciaY,delta)):
+			voltando=false
+
+func mover(distanciaX,distanciaY,delta):
+	
+	var moduloX=0
+	var moduloY=0
+	var moveX=0
+	var moveY=0
+	
+	if(distanciaX>=0):
+			moduloX=distanciaX
+	else:
+		moduloX=-distanciaX
+		
+	if(distanciaY>=0):
+		moduloY=distanciaY
+	else:
+		moduloY=-distanciaY
+	
+	if((moduloX>=10)or(moduloY >=10)):
+		if(moduloY<=1):
+			moduloY=1
+		if(moduloX<=1):
+			moduloX=1
+		if(moduloX>moduloY):
+			moveX= distanciaX/moduloX
+			moveY= distanciaY/moduloX
+		else:
+			moveX = distanciaX/moduloY
+			moveY= distanciaY/moduloY
+		
+		set_position(Vector2(position.x+(moveX*delta*(speed+moduloX)),position.y+(moveY*delta*(speed+moduloY))))
+	else:
+		return true
+	return false
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if(anim_name=="atacando"):
+		golpesRealizados+=1
+		
+	
+	pass 
