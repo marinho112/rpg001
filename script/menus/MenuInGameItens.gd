99eaed26230742b0fb97e37c1b210648fc18a562
marinho112 <34494111+marinho112@users.noreply.github.com>
2020-05-20 21:43:11 -0300

    tentando fazer a barra de rolagem funcionar

--- script/menus/MenuInGameItens.gd
+++ script/menus/MenuInGameItens.gd
@@ -29,7 +29,24 @@ func _process(delta):
 			descerLista()
 		elif Input.is_action_just_pressed("ui_up"):
 			subirLista()
+		if Input.is_action_just_pressed("mouse_left"):
+			if na_area($fundo/AreaRolagem) :
+				pass
+			elif na_area($fundo/AreaRolagemBntCima) :
+				subirLista()
+			elif na_area($fundo/AreaRolagemBntBaixo) :
+				descerLista()
 
+
+func na_area(objeto):
+	var posicao = objeto.get_global_position()
+	var tamanho = objeto.get_node("Collision").get_shape().get_extents()
+	var mousePosicao = get_global_mouse_position()
+	tamanho = tamanho/2.0
+	var retorno =((mousePosicao.x < (posicao.x+tamanho.x))and(mousePosicao.x > (posicao.x-tamanho.x)))
+	retorno = (retorno and ((mousePosicao.y < (posicao.y+tamanho.y)) and (mousePosicao.y > (posicao.y-tamanho.y))))
+	return retorno
+	
 func desenhaCursor():
 	var posicaoX = $cursor.get_position().x
 	var posicaoY = listaItensObjeto[posicao].get_position().y
@@ -54,6 +71,15 @@ func desenharLista():
 	for i in len(listaItensObjeto):
 		var objeto = listaItensObjeto[i]
 		objeto.defItem(listaItens[i+primeiro])
+	#ARRUMAR POSICAO DA BARRA!!
+	var posicaoCursorBarra = $fundo/barraRolagem.get_global_position()
+	var posicaoBarra = $fundo/AreaRolagem/Collision.get_global_position()
+	var tamanhoBarra = $fundo/AreaRolagem/Collision.get_shape().get_extents()
+	var dif = (len(listaItens) - len(listaItensObjeto))
+	var localNovo = tamanhoBarra.y * (primeiro) / dif
+	posicaoCursorBarra.y = localNovo + posicaoBarra.y - (tamanhoBarra.y/2.0)
+	$fundo/barraRolagem.set_global_position(posicaoCursorBarra)
+	
 	desenhaCursor()
 
 func descerLista():
